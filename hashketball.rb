7940a1a93cefecb7599d1ee4649f21b2880984b9
Ishmet <ishmetjahan@yahoo.com>
2019-06-24 14:54:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -133,7 +133,7 @@ end
 
 def num_points_scored(name)
  
-   find_player = players.find {|player| player.fetch(:player_name) == name }
+   find_player = players.find {|player| player[:player_name] == name }
     #binding.pry
   result = find_player[:points]
   result.to_i
